a7b9a6b2323debea75244143b33e011317fd7749
Sarah Aldous <sealdous@gmail.com>
2019-06-10 16:30:53 -0700

    project info displaying on page

--- tutorial-part-four/src/components/layout.js
+++ tutorial-part-four/src/components/layout.js
@@ -1,74 +1,7 @@
 import React from "react"
-import { css } from "@emotion/core"
-import { useStaticQuery, Link, graphql } from "gatsby"
-
-import { rhythm } from "../utils/typography"
 
 export default ({ children }) => {
-    const data = useStaticQuery(
-        graphql`
-        query {
-
-
-        allAirtable {
-            edges {
-              node {
-                data {
-                  Your_Name
-                  Project_Type
-                  Project_Name
-                  Project_Link
-                  Link_To_Portfolio
-                }
-              }
-            }
-          }
-        }
-        `
-
-      )
-      console.log(data)
-      const projects = data.allAirtable.edges.map (item => {
-          const project = item.node
-          console.log(project)
-      })
-      
-      return (
-
-  <div
-  
-  
-    css={css`
-      margin: 0 auto;
-      max-width: 700px;
-      padding: ${rhythm(2)};
-      padding-top: ${rhythm(1.5)};
-    `}
-  >
-    <h1>props.{project.Project_Name}</h1>
-    <h3>by {project.Your_Name}</h3>
-    <p>{project.Project_Type}</p>
-
-    <Link to={`/`}>
-      <h3
-        css={css`
-          margin-bottom: ${rhythm(2)};
-          display: inline-block;
-          font-style: normal;
-        `}
-      >
-      </h3>
-    </Link>
-    <Link
-      to={`/about/`}
-      css={css`
-        float: right;
-      `}
-    >
-      About
-    </Link>
-    {children}
-  </div>
-      
-    )
+   return (
+     <div>{children}</div>
+   )
 }
--- tutorial-part-four/src/pages/projects.js
+++ tutorial-part-four/src/pages/projects.js
@@ -1,31 +1,77 @@
 import React from "react"
+import { css } from "@emotion/core"
+import { useStaticQuery, Link, graphql } from "gatsby"
+
+import { rhythm } from "../utils/typography"
 import Layout from "../components/layout"
 
-export default () => (
-  <Layout>
+export default () => {
+  
    
-    <div>
-        <h1>Project Page</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>relativePath</th>
-              <th>prettySize</th>
-              <th>extension</th>
-              <th>birthTime</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.allFile.edges.map(({ node }, index) => (
-              <tr key={index}>
-                <td>{node.relativePath}</td>
-                <td>{node.prettySize}</td>
-                <td>{node.extension}</td>
-                <td>{node.birthTime}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table> */}
-      </div>
-  </Layout>
-)
+   const data = useStaticQuery(
+        graphql`
+        query {
+          allAirtable {
+              edges {
+                node {
+                  recordId 
+                  data {
+                    Your_Name
+                    Project_Type
+                    Project_Name
+                    Project_Link
+                    Link_To_Portfolio
+                  }
+                }
+              }
+            }
+          }
+        `
+
+      )
+      console.log(data)
+      const projects = data.allAirtable.edges.map (project => (
+          <div key={project.node.recordId}>
+            <h1><a href={project.node.data.Project_Link} target="_blank">{project.node.data.Project_Name}</a></h1>
+            <h3>by {project.node.data.Your_Name}</h3>
+            <p>{project.node.data.Project_Type}</p>
+          </div>
+        )
+      )
+      
+      return (
+        <Layout>
+          <div
+            css={css`
+              margin: 0 auto;
+              max-width: 700px;
+              padding: ${rhythm(2)};
+              padding-top: ${rhythm(1.5)};
+            `}
+          >
+            {projects}
+
+            <Link to={`/`}>
+              <h3
+                css={css`
+                  margin-bottom: ${rhythm(2)};
+                  display: inline-block;
+                  font-style: normal;
+                `}
+              >
+              </h3>
+            </Link>
+            <Link
+              to={`/about/`}
+              css={css`
+                float: right;
+              `}
+            >
+              About
+            </Link>
+           
+          </div>
+    
+        </Layout>
+      )
+}
