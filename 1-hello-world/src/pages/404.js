d67fc21c0c43efe6c03233e746afcd0515e6425e
Sarah Aldous <sealdous@gmail.com>
2019-08-27 12:37:27 -0700

    hello world starter site through tutorial part 1

---
+++ 1-hello-world/src/pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react'
+ import Header from "../components/header"
+
+ export default () => (
+    <div>
+       <Header headerText="Wrong Page, Dude" />
+       <img src="https://images.unsplash.com/photo-1459745930869-b3d0d72c3cbb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60" height="400px"/>
+    </div>
+ )
---
+++ 1-hello-world/src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Header from "../components/header"
+export default () => (
+    <div style={{ color: `teal` }}>
+     <Header headerText="About Gatsby" muffins="and muffins"/> 
+     <Header headerText="It's Pretty Cool" />
+     <Header headerText="Eva is the coolest" />
+      <p>Such wow. Very React.</p>
+    </div>
+)
