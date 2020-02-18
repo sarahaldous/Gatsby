94258f2d1058103bfe4629aed9dac294d1d99d8a
Sarah Aldous <sealdous@gmail.com>
2020-02-18 14:41:22 -0800

    added header and footer

---
+++ beekeeping/tutorial-part-three/src/components/footer.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default () => (
+    <h4>This is a footer.</h4>
+)
--- beekeeping/tutorial-part-three/src/components/layout.js
+++ beekeeping/tutorial-part-three/src/components/layout.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Header from '../components/header'
+import Footer from '../components/footer'
 import {Link} from 'gatsby'
 const ListLink = props => (
     <li style={{display: `inline-block`, marginRight: `1rem`}}>
@@ -7,6 +9,7 @@ const ListLink = props => (
 )
 export default ({ children }) => (
     <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+        <Header />
         <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
             <h3 style={{ display: `inline` }}>My Sweet Site</h3>
             </Link>
@@ -16,5 +19,6 @@ export default ({ children }) => (
             <ListLink to="/contact/">Contact</ListLink>
         </ul>
         {children}
+        <Footer />
     </div>
 )
